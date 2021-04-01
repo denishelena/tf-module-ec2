1d7733d9eadf870202001ab0ca4b7fd4f3a0b3f5
Denis Marcelo Santa Helena <denismst@gmail.com>
2021-04-01 10:03:52 -0300

    added ec2 module

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  subnet_id     = var.subnet_id
+  tags = {
+    Name = var.tagname
+  }
+
+  dynamic "ebs_block_device" {
+    for_each = var.ebs_blocks
+    content {
+      device_name = ebs_block_device.value["device_name"]
+      volume_size = ebs_block_device.value["volume_size"]
+      volume_type = ebs_block_device.value["volume_type"]
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "tagname" {
+  type    = string
+  default = "teste"
+}
+variable "app_port" {
+  type    = string
+  default = "80"
+}
+variable "subnet_id" {
+  type    = string
+  default = "subnet-e0d9bb84"
+}
+
+variable "ebs_blocks" {
+  type = list(object({
+    device_name = string
+    volume_size = string
+    volume_type = string
+  }))
+}
